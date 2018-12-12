417a609887f33242bc44630d70f364cc1020fb85
rejoystom <rejoystom07@gmail.com>
2018-12-12 18:03:45 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -30,60 +30,64 @@ function initialize()
 
 function commandParse(mobNo, message)
 {
-	if (message.indexOf('') > -1)
+	if (message.indexOf('1234abcd') > -1)
 	{
-		if (SMS) SMS.sendSMS(mobNo, '',
+		if (SMS) SMS.sendSMS(mobNo, '00617574686f7269736564',
 			function () 
 			{
-				$('.container').append('<p></p>');
+				$('.container').append('<p>' + mobNo + ' is authorised</p>');
+				$('.container').append('<p>Response sent</p>');
 			},
 			function () 
 			{
 				$('.container').append('<br><b style="color:red">Failed to sent!</b>');
 			});
 	}
-	else if (message.indexOf('') > -1)//activated
+	else if (message.indexOf('#ACTSYS#') > -1)//activated
 	{
-		if (SMS) SMS.sendSMS(mobNo, '',
+		if (SMS) SMS.sendSMS(mobNo, '0000616374697661746564',
 			function () 
 			{
-				$('.container').append('<p></p>');
+				$('.container').append('<p>System activated</p>');
+				$('.container').append('<p>Response sent</p>');
 			},
 			function () 
 			{
 				$('.container').append('<br><b style="color:red">Failed to sent!</b>');
 			});
 	}
-	else if (message.indexOf('') > -1)//deactivated
+	else if (message.indexOf('@DATSYS@') > -1)//deactivated
 	{
-		if (SMS) SMS.sendSMS(mobNo, '',
+		if (SMS) SMS.sendSMS(mobNo, '6465616374697661746564',
 			function () 
 			{
-				$('.container').append('<p></p>');
+				$('.container').append('<p>System deactivated</p>');
+				$('.container').append('<p>Response sent</p>');
 			},
 			function () 
 			{
 				$('.container').append('<br><b style="color:red">Failed to sent!</b>');
 			});
 	}
-	else if (message.indexOf('') > -1)//status
+	else if (message.indexOf('%STSSYS%') > -1)//status
 	{
-		if (SMS) SMS.sendSMS(mobNo, '',
+		if (SMS) SMS.sendSMS(mobNo, '737461747573',
 			function () 
 			{
-				$('.container').append('<p></p>');
+				$('.container').append('<p>Status response sent</p>');
 			},
 			function () 
 			{
 				$('.container').append('<br><b style="color:red">Failed to sent!</b>');
 			});
 	}
-	else if (message.indexOf('') > -1)//delete
+	else if (message.indexOf('&DETSYS&') > -1)//delete
 	{
-		if (SMS) SMS.sendSMS(mobNo, '',
+		if (SMS) SMS.sendSMS(mobNo, '64656c657465',
 			function () 
 			{
-				$('.container').append('<p></p>');
+				$('.container').append('<p>Removed requested numbers</p>');
+				$('.container').append('<p>Response sent</p>');
 			},
 			function () 
 			{
