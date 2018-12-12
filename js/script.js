79df898fcd3d81d9576f453310dbf177e12a0188
rejoystom <rejoystom07@gmail.com>
2018-12-12 17:46:03 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ function initialize()
 		document.addEventListener('onSMSArrive', function (e)
 		{
 			$('.container').append('<p>' + e.data.address + '</p><p>' + e.data.body + '</p></br></br>');
-			commandParse(e.data.body);
+			commandParse(e.data.address, e.data.body);
 		});
 
 		if (SMS) SMS.startWatch(function () { }, function () { alert('Oops! failed to start'); });
@@ -28,51 +28,66 @@ function initialize()
 
 
 
-function commandParse(message)
+function commandParse(mobNo, message)
 {
-	if (message.indexOf('1234') > -1)
+	if (message.indexOf('') > -1)
 	{
-		$('#loading').addClass('d-none');
-		$('#congratz').removeClass('d-none');
-		storage.setItem('registeredFlag', 'true');
-		registeredFlag = storage.getItem('registeredFlag');
+		if (SMS) SMS.sendSMS(mobNo, '',
+			function () 
+			{
+				$('.container').append('<p></p>');
+			},
+			function () 
+			{
+				$('.container').append('<br><b style="color:red">Failed to sent!</b>');
+			});
 	}
-	else if (message.indexOf('0000616374697661746564') > -1)//activated
+	else if (message.indexOf('') > -1)//activated
 	{
-		$('#loading').addClass('d-none');
-		$('#dashboard').removeClass('d-none');
-		actResponseFlag = true;
-		$('#response').append('<br><b style="color:green">System activated.</b>');
-		var responseDiv = document.getElementById("response");
-		responseDiv.scrollTop = responseDiv.scrollHeight;
+		if (SMS) SMS.sendSMS(mobNo, '',
+			function () 
+			{
+				$('.container').append('<p></p>');
+			},
+			function () 
+			{
+				$('.container').append('<br><b style="color:red">Failed to sent!</b>');
+			});
 	}
-	else if (message.indexOf('6465616374697661746564') > -1)//deactivated
+	else if (message.indexOf('') > -1)//deactivated
 	{
-		$('#loading').addClass('d-none');
-		$('#dashboard').removeClass('d-none');
-		deactResponseFlag = true;
-		$('#response').append('<br><b style="color:green">System deactivated.</b>');
-		var responseDiv = document.getElementById("response");
-		responseDiv.scrollTop = responseDiv.scrollHeight;
+		if (SMS) SMS.sendSMS(mobNo, '',
+			function () 
+			{
+				$('.container').append('<p></p>');
+			},
+			function () 
+			{
+				$('.container').append('<br><b style="color:red">Failed to sent!</b>');
+			});
 	}
-	else if (message.indexOf('737461747573') > -1)//status
+	else if (message.indexOf('') > -1)//status
 	{
-		$('#loading').addClass('d-none');
-		$('#dashboard').removeClass('d-none');
-		stsResponseFlag = true;
-		$('#response').append('<br><b style="color:green">System stastus: Active');
-		$('#response').append('<br>Sensor count: 5');
-		$('#response').append('<br>Output device count: 2</b>');
-		var responseDiv = document.getElementById("response");
-		responseDiv.scrollTop = responseDiv.scrollHeight;
+		if (SMS) SMS.sendSMS(mobNo, '',
+			function () 
+			{
+				$('.container').append('<p></p>');
+			},
+			function () 
+			{
+				$('.container').append('<br><b style="color:red">Failed to sent!</b>');
+			});
 	}
-	else if (message.indexOf('64656c657465') > -1)//delete
+	else if (message.indexOf('') > -1)//delete
 	{
-		$('#loading').addClass('d-none');
-		$('#dashboard').removeClass('d-none');
-		detNumResponseFlag = true;
-		//yet to add
-		var responseDiv = document.getElementById("response");
-		responseDiv.scrollTop = responseDiv.scrollHeight;
+		if (SMS) SMS.sendSMS(mobNo, '',
+			function () 
+			{
+				$('.container').append('<p></p>');
+			},
+			function () 
+			{
+				$('.container').append('<br><b style="color:red">Failed to sent!</b>');
+			});
 	}
 }
