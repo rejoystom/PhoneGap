c57b4f62628f06d55212cfa4335b9d6ef037817e
rejoystom <rejoystom07@gmail.com>
2019-01-18 19:07:17 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -53,42 +53,41 @@ function commandParse(message) {
 	if (message.indexOf('00617574686f7269736564') > -1) //authorised
 	{
 		$('#loading').addClass('d-none');
-		$('#congratz').removeClass('d-none');
+		$('#success').removeClass('d-none');
 		storage.setItem('registeredFlag', 'true');
-		registeredFlag = storage.getItem('registeredFlag');
 	} else if (message.indexOf('0000616374697661746564') > -1) //activated
 	{
 		$('#loading').addClass('d-none');
 		$('#dashboard').removeClass('d-none');
 		actResponseFlag = true;
-		$('#response').append('<br><b style="color:green">System activated.</b>');
-		var responseDiv = document.getElementById("response");
+		$('.dbResponse').append('<p style="color:green">System activated.</p>');
+		var responseDiv = document.getElementsByClassName("response");
 		responseDiv.scrollTop = responseDiv.scrollHeight;
 	} else if (message.indexOf('6465616374697661746564') > -1) //deactivated
 	{
 		$('#loading').addClass('d-none');
 		$('#dashboard').removeClass('d-none');
 		deactResponseFlag = true;
-		$('#response').append('<br><b style="color:green">System deactivated.</b>');
-		var responseDiv = document.getElementById("response");
+		$('.dbResponse').append('<p style="color:green">System deactivated.</p>');
+		var responseDiv = document.getElementsByClassName("response");
 		responseDiv.scrollTop = responseDiv.scrollHeight;
 	} else if (message.indexOf('737461747573') > -1) //status
 	{
 		$('#loading').addClass('d-none');
 		$('#dashboard').removeClass('d-none');
 		stsResponseFlag = true;
-		$('#response').append('<br><b style="color:green">System stastus: Active');
-		$('#response').append('<br>Sensor count: 5');
-		$('#response').append('<br>Output device count: 2</b>');
-		var responseDiv = document.getElementById("response");
+		$('.dbResponse').append('<p style="color:green">System stastus: Active');
+		$('.dbResponse').append('<br>Sensor count: 5');
+		$('.dbResponse').append('<br>Output device count: 2</p>');
+		var responseDiv = document.getElementsByClassName("response");
 		responseDiv.scrollTop = responseDiv.scrollHeight;
 	} else if (message.indexOf('64656c657465') > -1) //delete
 	{
 		$('#loading').addClass('d-none');
 		$('#dashboard').removeClass('d-none');
 		detNumResponseFlag = true;
-		$('#response').append('<br><b style="color:green">Removed requested numbers</b>');
-		var responseDiv = document.getElementById("response");
+		$('.dbResponse').append('<p style="color:green">Removed requested numbers</p>');
+		var responseDiv = document.getElementsByClassName("response");
 		responseDiv.scrollTop = responseDiv.scrollHeight;
 	}
 }
@@ -152,4 +151,8 @@ function slide(dir) {
 		default:
 			break;
 	}
+}
+
+function closeSettings() {
+	$('#settings').addClass('d-none');
 }
